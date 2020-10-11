6c5fff3238b822f703cb9cf717e2bf6b1e81f6e2
BillyBDev <BillyBDev@gmail.com>
2020-10-11 10:28:47 -0700

    Added delete functionality for list items.

--- src/List.js
+++ src/List.js
@@ -13,11 +13,18 @@ const AddItemBtn = (props) => {
 }
 
 const ItemsList = (props) => {
-    const list = props.items.map((item) => {
+    const list = props.items.map((item, index) => {
         return (
-            <li className="incomeListItem">{item.desc} - {item.amount}</li>
+            <li className="incomeListItem">
+                {item.desc} - {item.amount}  
+                <div className="delet">
+                    <button className="ui mini circular icon button inverted red" onClick={() => props.removeItem(index, item.amount)}>
+                        <i className="icon minus" />
+                    </button>
+                </div>
+            </li>
         )
-    });
+    })
 
     return <ul>{list}</ul>
 }
@@ -32,7 +39,7 @@ class List extends Component {
     toggleForm = () => {
         this.setState({
             showForm: !this.state.showForm
-        })
+        });
     }
 
     addItem = (obj) => {
@@ -42,11 +49,20 @@ class List extends Component {
             items: [...this.state.items, newItem]
         });
     }
+
+    removeItem = (index, amount) => {
+        const items = this.state.items;
+
+        this.props.adjustTotal(amount * -1);
+        this.setState({items: items.filter((item, i) => {
+            return i !== index
+        })})
+    }
     
     render(){
         return (
             <div>
-                <ItemsList items={this.state.items} />
+                <ItemsList items={this.state.items} removeItem={this.removeItem} />
                 <Form 
                     showForm={this.state.showForm} 
                     toggleForm={this.toggleForm}
