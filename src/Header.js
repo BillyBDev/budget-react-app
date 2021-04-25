5d546268d1c2cf4341b05002db53d2b01d77a46d
BillyBDev <BillyBDev@gmail.com>
2021-04-25 14:26:30 -0700

    Fixed negative currency formatting

--- src/Header.js
+++ src/Header.js
@@ -1,15 +1,25 @@
 import React from 'react';
 
+function numToAmt(num){
+    let i = num;
+    if (num >= 0){
+        return "$" + i.toFixed(2);
+    } else {
+        i *= -1;
+        return "-$" + i.toFixed(2);
+    }
+}
+
 function pocketAmt(num){
     let ans = num / 30;
-    return ans.toFixed(2)
+    return ans;
 }
 
 function DailyAmount(props){
     return (
         <div id="day-label">
             Per day: <span id="day-amt" class={props.colorClass}>
-                ${pocketAmt(props.total)}
+                {numToAmt(pocketAmt(props.total))}
             </span>
         </div>
     )
@@ -17,14 +27,14 @@ function DailyAmount(props){
 
 const Header = (props) => {
     let colorClass;
-    if (props.total > 0){
-        colorClass = "positive"
+    if (props.total >= 0){
+        colorClass = "positive";
     } else if (props.total < 0){
-        colorClass="negative"
+        colorClass="negative";
     }
     return (
         <div class="header">
-            <div id="net-label">Net: <span id="net-amt" class={colorClass}>${props.total.toFixed(2)}</span></div>
+            <div id="net-label">Net: <span id="net-amt" class={colorClass}>{numToAmt(props.total)}</span></div>
             <DailyAmount total={props.total} colorClass={colorClass}/>
         </div>
     )
